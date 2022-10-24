215f48b09eb2d9fb7376fcb627329404ac3cd780
Volkan Öztoklu <volkanoztoklu@gmail.com>
2022-10-25 01:01:58 +0300

    logger is okay!

--- app.js
+++ app.js
@@ -23,7 +23,6 @@ app.use(cookieParser())
 
 //Routes
 app.get('/login', function (req, res) {
-  logger.userlogger.log('info', "sdasdyhasd") 
   res.cookie('jwt', '',{maxAge:1})
   res.sendFile(path.join(__dirname+'/views/login.html'))
 })
@@ -31,19 +30,15 @@ app.get('/register', function (req, res) {
   res.cookie('jwt', '',{maxAge:1})
   res.sendFile(path.join(__dirname+'/views/register.html'))
 })
-app.get('/deneme', async function(req, res) { 
-  
-})
 app.post('/register', async function(req, res) {
   try {
-    console.log(req.body)
     //Creating user in DB
     const text = "INSERT INTO users(name, surname, email, password, register_date) VALUES($1, $2, $3, crypt($4, gen_salt('bf')), $5) RETURNING *"
     const  now_date = new Date()
     const values = [req.body.name, req.body.surname, req.body.email, req.body.password, now_date]
     const { rows } = await postgresClient.query(text, values)
     console.log("User is created.")
-    
+    logger.register_logger.log('info', `${req.body.email} is registered!`) 
     // Creating TOKEN
     const {name, surname, email} =req.body
     const token =jwt.sign({
@@ -52,25 +47,24 @@ app.post('/register', async function(req, res) {
       email: email,
       exp: Math.floor(Date.now() / 1000) + (60*60*24)
     }, 'secretKey')
-    console.log(token)
     console.log("JWT TOKEN is created.")
     res.cookie('jwt', token, {httpOnly: true,maxAge: 60*60*24*1000})
     return res.status(200).redirect("/login")    
 
   } catch (error) {
+    logger.register_logger.log('error', `${req.body.email} have error that occured ${error.message}`) 
     console.log("Error occured", error.message)
     return res.status(400).json({message:error.message})
   }
 })
 app.post('/login', async function(req, res) {
   try {
-    console.log(req.body) 
     const text = "SELECT * FROM users WHERE email =$1 AND password = crypt($2, password)"
     const values = [req.body.email, req.body.password]
     const { rows } = await postgresClient.query(text, values)
-    console.log(rows)
     if (!rows.length) {
       console.log('User not found')
+      logger.login_logger.log('error', `email: ${req.body.email}, password : ${req.body.password} USER NOT FOUND!`) 
       return res.status(404).json({message: 'User not found'})
     }  
     // Creating TOKEN
@@ -82,10 +76,12 @@ app.post('/login', async function(req, res) {
       exp: Math.floor(Date.now() / 1000) + (60*60*24)
     }, 'secretKey')
     res.cookie('jwt', token, {httpOnly: true,maxAge: 60*60*24*1000})
+    logger.login_logger.log('info', `${req.body.email} logged in!`) 
     return res.status(200).redirect('/users')
      
   } catch (error) {
     console.log("Error occured", error.message)
+    logger.login_logger.log('error', `${req.body.email} have a error that occured ${error.message}`) 
     return res.status(400).json({message: error.message})
   } 
 })
@@ -110,26 +106,29 @@ app.post('/updating/:paramId', checkJwt , async function(req, res) {
   try {
     const text = "SELECT * FROM users WHERE id = $1 AND password = crypt($2, password)"
     const values = [req.params.paramId, req.body.oldpassword]
-    const { rows } = await postgresClient.query(text, values)
-    console.log(rows)
+    const {rows} = await postgresClient.query(text, values)
     if (!rows.length) {
       console.log('User not found')
+      logger.update_logger.log('error', `${rows[0].email} not found!`) 
       return res.status(404).json({message: 'User not found'})
     } 
     if (req.body.newpassword == '') {
       const text_for_update = "UPDATE users SET name = $1, surname = $2, email = $3 WHERE id = $4 RETURNING*"
       const values_for_update = [req.body.name, req.body.surname, req.body.email, req.params.paramId]
-      const { rows } = await postgresClient.query(text_for_update, values_for_update)
+      await postgresClient.query(text_for_update, values_for_update)
+      logger.update_logger.log('info', `${rows[0].name},${rows[0].surname},${rows[0].email} are changed with ${req.body.name},${req.body.surname},${req.body.email}`) 
       res.redirect("/users")
     } else {
       const text_for_update = "UPDATE users SET name = $1, surname = $2, email = $3, password = crypt($4,password) WHERE id = $5 RETURNING*"
       const values_for_update = [req.body.name, req.body.surname, req.body.email, req.body.newpassword, req.params.paramId]
-      const { rows } = await postgresClient.query(text_for_update, values_for_update)
+      await postgresClient.query(text_for_update, values_for_update)
+      logger.update_logger.log('info', `${rows[0].name},${rows[0].surname},${rows[0].email} are changed with ${req.body.name},${req.body.surname},${req.body.email}`) 
       res.redirect("/users")
     }
 
   } catch (error) {
     console.log("Error occured", error.message)
+    logger.update_logger.log('error', `${rows[0].email} have a error that occured ${error.message}`) 
     return res.status(400).json({message:error.message})
   }
 })
@@ -138,14 +137,16 @@ app.get('/user/delete/:paramId', checkJwt , async function(req, res) {
     const text = "DELETE FROM users WHERE id = $1 RETURNING *"
     const value = [req.params.paramId]
     const { rows } = await postgresClient.query(text, value)
-    console.log(rows)
-    res.redirect("/users")
     if (!rows.length) {
       console.log('User not found')
+      logger.delete_logger.log('error', `${rows[0].email} is not found.`) 
       return res.status(404).json({message: 'User not found'})
-    } 
+    }
+    logger.delete_logger.log('info', `${rows[0].email} is deleted.`) 
+    res.redirect("/users")
   } catch (error) {
     console.log("Error occured", error.message)
+    logger.delete_logger.log('error', `${rows[0].email} have a error that occured ${error.message}`) 
     return res.status(400).json({message:error.message})
   }
 })
--- logger.js
+++ logger.js
@@ -1,17 +1,56 @@
 const winston = require('winston');
 
-const userlogger = winston.createLogger({
+
+const login_logger = winston.createLogger({
   level: 'info',
-  format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
+  format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
   transports: [
     //
     // - Write all logs with importance level of `error` or less to `error.log`
     // - Write all logs with importance level of `info` or less to `combined.log`
     //
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'deneme.log' }),
+    new winston.transports.File({ filename: './log_files/login.log', level: 'error'}),
+    new winston.transports.File({ filename: './log_files/login.log'}),
   ],
 });
 
-module.exports = {userlogger}
+const register_logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+    transports: [
+      //
+      // - Write all logs with importance level of `error` or less to `error.log`
+      // - Write all logs with importance level of `info` or less to `combined.log`
+      //
+      new winston.transports.File({ filename: './log_files/register.log', level: 'error'}),
+      new winston.transports.File({ filename: './log_files/register.log'}),
+    ],
+  });
+
+  const delete_logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+    transports: [
+      //
+      // - Write all logs with importance level of `error` or less to `error.log`
+      // - Write all logs with importance level of `info` or less to `combined.log`
+      //
+      new winston.transports.File({ filename: './log_files/delete.log', level: 'error'}),
+      new winston.transports.File({ filename: './log_files/delete.log'}),
+    ],
+  });
+
+  const update_logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+    transports: [
+      //
+      // - Write all logs with importance level of `error` or less to `error.log`
+      // - Write all logs with importance level of `info` or less to `combined.log`
+      //
+      new winston.transports.File({ filename: './log_files/update.log', level: 'error'}),
+      new winston.transports.File({ filename: './log_files/update.log'}),
+    ],
+  });
+
+module.exports = {login_logger, register_logger, delete_logger, update_logger}
